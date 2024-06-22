a0dc5aa9590a60cbe298b4ac71769f8bb6bbeb2f
RAYNZ13 <im.sra13@gmail.com>
2024-06-22 10:54:53 +0530

    middleware is used

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const dotevn = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/contacts', (req,res) => {
-    res.json("Hi from GET Method");
-})
+app.use("/api/contacts", require('./routes/contactRoutes'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
